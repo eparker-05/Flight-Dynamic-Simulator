84f235c4ed26992ad76e34378cb8bccb0073b113
Elliot Parker <eaparker.05@gmail.com>
2026-04-01 13:09:20 -0400

    Update Aerodynamics.cpp

--- src/Aerodynamics.cpp
+++ src/Aerodynamics.cpp
@@ -2,33 +2,37 @@
 #include <cmath>
 #include "AircraftParameters.h" //Including structure of parameters
 #include "AircraftState.h" //Including structure of the states
+#include "AerodynamicForces.h"
 using namespace std;
 //Function Declarations 
 AircraftParameters f5;
 double compute_velocity(const AircraftState& state);
 double compute_beta(double, const AircraftState& state);
 double compute_alpha(const AircraftState& state);
-double compute_CL(double);
+double compute_CL(double, double);
 double compute_CD(double);
 double compute_CY(double);
 double compute_drag(double, double);
 double compute_lift(double, double);
 double compute_sideforce(double, double);
+double compute_Cm(double, double, double, double);
+double compute_pitch_moment(double, double);
+AerodynamicForces compute_forces(const AircraftState& state);
 
-
-int main(){
+void print(){
     AircraftState state; //defining state variable to pass through functions
 
     //Setting test velocity components
-    state.u = 180.0; // m/s
-    state.v = 0.01; // m/s
-    state.w = 6.0; // m/s
+    state.u = 179.932; // m/s
+    state.v = 0.0; // m/s
+    state.w = 4.951; // m/s
+    double delta_e = 0.0133725;
 
     //Function calls
     double V = compute_velocity(state);
     double beta = compute_beta(V, state);
     double alpha = compute_alpha(state);
-    double CL = compute_CL(alpha);
+    double CL = compute_CL(alpha, delta_e);
     double CD = compute_CD(CL);
     double CY = compute_CY(beta);
     double drag = compute_drag(CD, V);
@@ -46,9 +50,10 @@ int main(){
     cout << "The lift is " << lift << "N."<< endl;
     cout << "The sideforce is " << sideforce << "N." << endl;
 
-return 0;
+
+//return 0;
 }
-//All function definitions
+//-----------Velocity-------------
 double compute_velocity(const AircraftState& state)
 {
     double V = sqrt(state.u * state.u +
@@ -56,18 +61,25 @@ double compute_velocity(const AircraftState& state)
                     state.w * state.w);
     return V;
 }
+
+//-----------Angles----------
+
 double compute_beta(double V, const AircraftState& state)
 {
-    return asin(state.v/state.u);
+    return asin(state.v/V);
 }
 double compute_alpha(const AircraftState& state)
 {
     return atan2(state.w, state.u);
 }
-double compute_CL(double alpha) //We use alpha instead of trim_alpha to create generic functions
+
+//------------Aerodynamic Coefficients---------------
+
+double compute_CL(double alpha, double delta_e)
 {
-    return f5.CL0 + f5.CL_alpha * alpha;
+    return f5.CL0 + f5.CL_alpha * alpha + f5.CL_delta_e * delta_e;
 }
+//^We use alpha instead of trim_alpha to create generic functions^
 double compute_CD(double CL)
 {
     double k = 0.05;
@@ -77,6 +89,9 @@ double compute_CY(double beta)
 {
     return f5.CY_beta * beta;
 }
+
+//------------Forces------------
+
 double compute_drag(double CD, double V)
 {
     return 0.5 * f5.rho * V * V * f5.wingArea * CD;
@@ -88,4 +103,19 @@ double compute_lift(double CL, double V)
 double compute_sideforce(double CY, double V)
 {
     return 0.5 * f5.rho * V * V * f5.wingArea * CY;
-} 
+}
+double compute_Cm(double alpha, double q, double V, double delta_e)
+{
+    double q_hat = 0.0;
+
+    if (V > 1e-8) {
+        q_hat = (f5.chord / (2.0 * V)) * q;
+    }
+
+    return f5.Cm0 + f5.Cm_alpha * alpha + f5.Cm_q * q_hat + f5.Cm_delta_e * delta_e;
+}
+
+double compute_pitch_moment(double Cm, double V)
+{
+    return 0.5 * f5.rho * V * V * f5.wingArea * f5.chord * Cm;
+}
