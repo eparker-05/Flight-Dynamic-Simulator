78b4919fd3e9b74fbdb0ac4188f1e1a20278e9f4
Elliot Parker <eaparker.05@gmail.com>
2026-04-14 12:05:45 -0400

    Update AircraftState.h

--- include/AircraftState.h
+++ include/AircraftState.h
@@ -1,1 +1,28 @@
+#ifndef Aircraft_State_h
+#define Aircraft_State_h
 
+struct AircraftState
+{
+    //Body velocities
+    double u;
+    double v; 
+    double w;
+
+    //Angular rates
+    double p;
+    double q;
+    double r;
+
+    //Orientation
+    double phi;
+    double theta;
+    double psi;
+
+    //Position
+    double x;
+    double y; 
+    double z;
+
+    
+};
+#endif 
